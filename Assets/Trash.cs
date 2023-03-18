53302dbb14fe0b547d8bc196e6c7e8a44e1c6b74
Joachim Hedenius (privat) <joachim@hedenius.se>
2023-03-18 16:16:22 +0100

    transparency in sprites

--- Assets/Trash.cs
+++ Assets/Trash.cs
@@ -1,19 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class Trash : MonoBehaviour
 {
 
     public bool falling = true;
-    
-    void OnCollisionEnter2D(Collision2D collision) => Log(collision);
-    void OnCollisionStay2D(Collision2D collision) => Log(collision);
-    void OnCollisionExit2D(Collision2D collision) => Log(collision);
+    public float RotateSpeedY = 0.01f;
+    public float RotateSpeedX = 0.01f;
+    public float RotateSpeedZ = 0.01f;
 
-    void Log(Collision2D collision, [CallerMemberName] string message = null)
+    private void OnEnable()
+    {
+        RotateSpeedY *= (1.0f - (2.0f*Random.value));
+        RotateSpeedX *= (1.0f - (2.0f*Random.value));
+        RotateSpeedZ *= (1.0f - (2.0f*Random.value));
+    }
+
+    private void Update()
+    {
+        transform.Rotate(RotateSpeedX, RotateSpeedY, RotateSpeedZ, Space.World);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) => Collide(collision);
+    //void OnCollisionStay2D(Collision2D collision) => Collide(collision);
+    //void OnCollisionExit2D(Collision2D collision) => Collide(collision);
+
+    void Collide(Collision2D collision, [CallerMemberName] string message = null)
     {
         Debug.Log($"{message} called on {name} because a collision with {collision.collider.name}");
         if (collision.collider.gameObject.GetComponent<Trash>() != null)
